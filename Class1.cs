e3cae587f4a923c059bcee925fa98e7e456721fd
Alain Leglise <fraternite76-alain@yahoo.fr>
2022-01-18 01:06:11 +0100

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ClassOne
+{
+    public class Class1
+    {
+        public Class1(){}
+
+        public int Run()
+        {
+            return 0;
+        }
+    }
+}
